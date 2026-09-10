e640ae5378eea450d7d3dc07abca40c28b60f298
Linhy19 <109963231+Linhy19@users.noreply.github.com>
2026-09-10 22:48:00 +0200

    Centralize public Supabase configuration

---
+++ axono-config.js
@@ -0,0 +1,4 @@
+window.AXONO_CONFIG = Object.freeze({
+  supabaseUrl: 'https://wdwmjjbcphntymodnbub.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indkd21qamJjcGhudHltb2RuYnViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyMjk0NDQsImV4cCI6MjA4ODgwNTQ0NH0.5Er4or14JJZveXexxO7XCaRIhguZ3FHuLWDJbyzB0oU'
+});
